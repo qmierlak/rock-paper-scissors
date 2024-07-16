9655c62f4a6e9617fa699e4ed365bcb0fee19a3b
Quinn <qmierlak@gmail.com>
2024-07-16 11:34:54 -0500

    Add getHumanChoice function

--- game.js
+++ game.js
@@ -4,7 +4,8 @@ function getComputerChoice() {
     // Generate a random number
     let random_value = makeRandomChoice(3);
 
-    console.log("My random value is: " + random_value);
+    // console.log("My random value is: " + random_value);
+
     // If number is 0, pick rock
     if (random_value == 0) {
         return "rock";
@@ -25,8 +26,25 @@ function makeRandomChoice(max) {
 }
 
 
+// Get the user's choice of rock, paper, or scissors
+function getHumanChoice() {
+    // Prompt the user for their selection
+    let choice = prompt("Please input rock, paper, or scissors (case-sensititve)");
+
+    // Check that their selection is valid
+    if (choice != ("rock" || "paper" || "scissors")) {
+        console.log("Sorry, you didn't input rock, paper, or scissors (remember that they're case-sensitive");
+        return;
+    }
+
+    // Print user's selection to console
+    console.log("You chose: " + choice);
+
+    // Return user's selection
+    return choice;
+}
 
 
 
-
+getHumanChoice();
 console.log(getComputerChoice());
