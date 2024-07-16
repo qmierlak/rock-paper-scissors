68dc3b9f30566fb35ac475c43d0dfd2647be8384
Quinn <qmierlak@gmail.com>
2024-07-16 11:41:06 -0500

    Make user's choice case insensitive

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+// Keeps track of the running score for both the human player and computer
+let humanScore = 0;
+let computerScore = 0;
+
+
 // Randomly returns "rock", "paper", or "scissors"
 function getComputerChoice() {
 
@@ -29,22 +34,33 @@ function makeRandomChoice(max) {
 // Get the user's choice of rock, paper, or scissors
 function getHumanChoice() {
     // Prompt the user for their selection
-    let choice = prompt("Please input rock, paper, or scissors (case-sensititve)");
-
-    // Check that their selection is valid
-    if (choice != ("rock" || "paper" || "scissors")) {
-        console.log("Sorry, you didn't input rock, paper, or scissors (remember that they're case-sensitive");
-        return;
-    }
+    let choice = prompt("Please input rock, paper, or scissors");
+    
+    // Make the user's input case insensitive
+    choice = choice.toLowerCase();
 
     // Print user's selection to console
     console.log("You chose: " + choice);
+
+    // Check that their selection is valid
+     if (choice != ("rock" || "paper" || "scissors")) {
+        console.log("Sorry, you didn't input rock, paper, or scissors");
+        return;
+    }
+
+    
 
     // Return user's selection
     return choice;
 }
 
+// Plays a single round of rock, paper, scissors
+// Increments the winner's score and logs a winner announcement
+function playRound(humanChoice, computerChoice) {
+    
+
+}
 
 
-getHumanChoice();
-console.log(getComputerChoice());
+// getHumanChoice();
+// console.log(getComputerChoice());
