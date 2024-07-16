a87372c880199548fa5109f9e40a55b892618bef
Quinn <qmierlak@gmail.com>
2024-07-16 11:29:58 -0500

    Add getComputerChoice function

--- game.js
+++ game.js
@@ -1,1 +1,32 @@
-console.log("Hi");
+// Randomly returns "rock", "paper", or "scissors"
+function getComputerChoice() {
+
+    // Generate a random number
+    let random_value = makeRandomChoice(3);
+
+    console.log("My random value is: " + random_value);
+    // If number is 0, pick rock
+    if (random_value == 0) {
+        return "rock";
+    }
+    // If number is 1, pick paper
+    if (random_value == 1) {
+        return "paper";
+    }
+    // If number is 2, pick scissors
+    if (random_value == 2) {
+        return "scissors";
+    }
+}
+
+// Randomly returns a number between 0 and max-1
+function makeRandomChoice(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+
+
+
+console.log(getComputerChoice());
